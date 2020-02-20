24f3b0eeed958df4fcd312b3b1fd189af4babb55
BHATI Vikramaditya <vbhati@enseirb-matmeca.fr>
2020-02-20 16:26:17 +0100

    Renaming GameObjects

--- Test Stage Unity Développeur/Assets/Algorithmique/RandomGeneration.cs
+++ Test Stage Unity Développeur/Assets/Algorithmique/RandomGeneration.cs
@@ -2,119 +2,122 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomGeneration : MonoBehaviour
+namespace RandomGeneration
 {
-    /// <summary>
-    /// container for the random numbers
-    /// </summary>
-    List<int> line = new List<int>();
-    int listCount = 30;
-
-    /// <summary>
-    /// This function generates random numbers between 0 and 3 (included) without any triplets
-    /// </summary>
-    public void GenerateRandomNumbers()
+    public class RandomGeneration : MonoBehaviour
     {
-        // 1st step : fill the array with random numbers
-        InitialRandomNumbers(0,4);        
-        while (IsRemovingElements())
-        {
-            // 2nd step : remove triplets
-            RemoveTriplets();
-            // 3rd step : fill the array and restart at step 2
-            FillLine(0,4);
-        }
-    }
+        /// <summary>
+        /// container for the random numbers
+        /// </summary>
+        List<int> line = new List<int>();
+        int listCount = 30;
 
-    /// <summary>
-    /// completely fills the array with random numbers (a inclusive, b exclusive)
-    /// </summary>
-    /// <param name="a">inclusive int</param>
-    /// <param name="b">exclusive int</param>
-    void InitialRandomNumbers(int a, int b)
-    {
-        line = new List<int>();
-        Debug.Log("Génération aléatoire");
-        for (int i = 0; i < listCount; i++)
+        /// <summary>
+        /// This function generates random numbers between 0 and 3 (included) without any triplets
+        /// </summary>
+        public void GenerateRandomNumbers()
         {
-            line.Add(Random.Range(a,b));
-        }
-        PrintLine();
-    }
-
-    /// <summary>
-    /// this function return a bool whether there are triplets or not
-    /// </summary>
-    /// <returns></returns>
-    bool IsRemovingElements()
-    {
-        bool restart = false;
-        int a = line[0];
-        int b = line[1];
-        for (int i = 2; i < line.Count; i++)
-        {
-            if (line[i]==a && line[i] == b)
+            // 1st step : fill the array with random numbers
+            InitialRandomNumbers(0,4);        
+            while (IsRemovingElements())
             {
-                restart = true;
-                break;
+                // 2nd step : remove triplets
+                RemoveTriplets();
+                // 3rd step : fill the array and restart at step 2
+                FillLine(0,4);
             }
-            a = b;
-            b = line[i];
         }
-        return restart;
-    }
 
-    /// <summary>
-    /// this function removes all triplets
-    /// </summary>
-    void RemoveTriplets()
-    {
-        Debug.Log("Suppression des groupes d’éléments identiques");
-        int a = line[line.Count-1];
-        int b = line[line.Count-2];
-        // backward traversal because there will be item suppression
-        for (int i = line.Count-3; i >= 0; i--)
+        /// <summary>
+        /// completely fills the array with random numbers (a inclusive, b exclusive)
+        /// </summary>
+        /// <param name="a">inclusive int</param>
+        /// <param name="b">exclusive int</param>
+        void InitialRandomNumbers(int a, int b)
         {
-            if (line[i]==a && line[i] == b)
+            line = new List<int>();
+            Debug.Log("Génération aléatoire");
+            for (int i = 0; i < listCount; i++)
             {
-                line.RemoveAt(i);
-                line.RemoveAt(i+1);
-                line.RemoveAt(i+2);
-                break;
+                line.Add(Random.Range(a,b));
             }
-            a = line[i+1];
-            b = line[i];
+            PrintLine();
         }
-        PrintLine();
-        if (IsRemovingElements()) // removing elements might have produced new triplets
-            RemoveTriplets();
-    }
+
+        /// <summary>
+        /// this function return a bool whether there are triplets or not
+        /// </summary>
+        /// <returns></returns>
+        bool IsRemovingElements()
+        {
+            bool restart = false;
+            int a = line[0];
+            int b = line[1];
+            for (int i = 2; i < line.Count; i++)
+            {
+                if (line[i]==a && line[i] == b)
+                {
+                    restart = true;
+                    break;
+                }
+                a = b;
+                b = line[i];
+            }
+            return restart;
+        }
+
+        /// <summary>
+        /// this function removes all triplets
+        /// </summary>
+        void RemoveTriplets()
+        {
+            Debug.Log("Suppression des groupes d’éléments identiques");
+            int a = line[line.Count-1];
+            int b = line[line.Count-2];
+            // backward traversal because there will be item suppression
+            for (int i = line.Count-3; i >= 0; i--)
+            {
+                if (line[i]==a && line[i] == b)
+                {
+                    line.RemoveAt(i);
+                    line.RemoveAt(i+1);
+                    line.RemoveAt(i+2);
+                    break;
+                }
+                a = line[i+1];
+                b = line[i];
+            }
+            PrintLine();
+            if (IsRemovingElements()) // removing elements might have produced new triplets
+                RemoveTriplets();
+        }
     
-    /// <summary>
-    /// fills the array with random numbers (a inclusive, b exclusive)
-    /// </summary>
-    /// <param name="a">inclusive int</param>
-    /// <param name="b">exclusive int</param>
-    void FillLine(int a, int b)
-    {
-        Debug.Log("Remplissage" + (listCount - line.Count));
-        while (line.Count != listCount)
+        /// <summary>
+        /// fills the array with random numbers (a inclusive, b exclusive)
+        /// </summary>
+        /// <param name="a">inclusive int</param>
+        /// <param name="b">exclusive int</param>
+        void FillLine(int a, int b)
         {
-            line.Add(Random.Range(a,b));
+            Debug.Log("Remplissage");
+            while (line.Count != listCount)
+            {
+                line.Add(Random.Range(a,b));
+            }
+            PrintLine();
         }
-        PrintLine();
-    }
 
-    /// <summary>
-    /// Use this function to display the line array in the console
-    /// </summary>
-    void PrintLine()
-    {
-        string num = "";
-        for (int i = 0; i < line.Count; i++)
+        /// <summary>
+        /// Use this function to display the line array in the console
+        /// </summary>
+        void PrintLine()
         {
-            num += line[i].ToString();
+            string num = "";
+            for (int i = 0; i < line.Count; i++)
+            {
+                num += line[i].ToString();
+            }
+            Debug.Log(num);
         }
-        Debug.Log(num);
     }
 }
